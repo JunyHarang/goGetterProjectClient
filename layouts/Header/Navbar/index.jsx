7d1d6ba109f5f2a214ccbabb6c28204414197118
seoyoungsoo <iulove37@naver.com>
2021-07-11 21:44:52 +0900

    Chore: 발표용 쪽지함 주석 처리

--- layouts/Header/Navbar/index.jsx
+++ layouts/Header/Navbar/index.jsx
@@ -78,13 +78,6 @@ const Navbar = () => {
                 </LinkClicked>
               </Link>
             </li>
-            <li>
-              <Link to="/note">
-                <LinkClicked isActive={pathname.includes('/note')}>
-                  <i class="far fa-bell fa-lg">{/* <span>●</span> */}</i>
-                </LinkClicked>
-              </Link>
-            </li>
           </ul>
         )}
       </Inner>
